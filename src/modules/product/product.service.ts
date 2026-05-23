277e046afa11980b48a5cc87c197c99f01218123
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-23 13:58:25 +0300

    feat(modules): add service for product modules

---
+++ src/modules/product/product.service.ts
@@ -0,0 +1,31 @@
+import { ProductRepository } from './product.repository';
+import { CreateInput, UpdateInput } from './product.types';
+
+export class ProductService {
+  constructor(private readonly repository: ProductRepository) {}
+
+  async getAll() {
+    return this.repository.findMany();
+  }
+
+  async getById(id: string) {
+    const product = await this.repository.findById(id);
+    if (!product) throw new Error('Product not found');
+    return product;
+  }
+
+  async create(data: CreateInput) {
+    const slug = data.name.toLowerCase().replace(/\s+/g, '-');
+    return this.repository.create({ ...data, slug });
+  }
+
+  async update(id: string, data: UpdateInput) {
+    await this.getById(id);
+    return this.repository.update(id, data);
+  }
+
+  async delete(id: string) {
+    await this.getById(id);
+    return this.repository.delete(id);
+  }
+}
