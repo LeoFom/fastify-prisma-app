56486cb035bff454e71340e7c084d5fb1e2624ab
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-23 13:53:14 +0300

    feat(plugins): update use jwtVerify with onlyCookie in authMiddleware

--- src/shared/plugins/auth.ts
+++ src/shared/plugins/auth.ts
@@ -5,9 +5,9 @@ export async function authMiddleware(
   reply: FastifyReply
 ) {
   try {
-    console.log("[authMiddleware] -> cookie - ", request.headers.cookie)
+    console.log("[authMiddleware] -> cookie - ", request.cookies)
 
-    await request.jwtVerify()
+    await request.jwtVerify({ onlyCookie: true });
 
     console.log("[authMiddleware] -> user - ", request.user)
     console.log("[authMiddleware] -> userId - ", request.user.userId)
