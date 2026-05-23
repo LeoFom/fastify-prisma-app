84eba04aecb4cf471fb57fb9fd3b6f07114be31c
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-23 14:01:07 +0300

    feat(modules): add repository for product modules

---
+++ src/modules/product/product.repository.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '../../generated/prisma/client';
+import { CreateInput, UpdateInput } from './product.types';
+
+export class ProductRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  // Посмотреть поле slug для data (CreateInput)
+  async create(data: CreateInput & { slug: string }) {
+    return this.prisma.product.create({ ...data });
+  }
+
+  async findMany() {
+    return this.prisma.product.findMany();
+  }
+
+  async findById(id: string) {
+    return this.prisma.product.findUnique({ where: { id } });
+  }
+
+  async update(id: string, data: UpdateInput) {
+    return this.prisma.product.update({ where: { id }, data });
+  }
+
+  async delete(id: string) {
+    return this.prisma.product.delete({ where: { id } });
+  }
+}
