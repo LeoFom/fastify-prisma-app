8337bfbdc54123b1208cf33889c30b739eca0db3
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-28 00:05:40 +0300

    feat(modules): update createProductsSchema and updateProductsSchema schema

--- src/modules/product/product.schema.ts
+++ src/modules/product/product.schema.ts
@@ -5,8 +5,8 @@ export const createProductsSchema = z.object({
   name: z.string().min(2),
   description: z.string().min(10).optional(),
   imageUrl: z.string().optional(),
-  category: z.enum(Object.values(ProductCategory) as [string, ...string[]]),
-  roast: z.enum(Object.values(RoastType) as [string, ...string[]]).optional(),
+  category: z.nativeEnum(ProductCategory),
+  roast: z.nativeEnum(RoastType).optional(),
   price: z.number().positive(),
 })
 
@@ -14,7 +14,7 @@ export const updateProductsSchema = z.object({
   name: z.string().min(2),
   description: z.string().min(10).optional(),
   imageUrl: z.string().optional(),
-  category: z.enum(Object.values(ProductCategory) as [string, ...string[]]),
-  roast: z.enum(Object.values(RoastType) as [string, ...string[]]).optional(),
+  category: z.nativeEnum(ProductCategory),
+  roast: z.nativeEnum(RoastType).optional(),
   price: z.number().positive(),
 }).partial();
