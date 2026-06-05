36fabfb3a75a4e7a5e6ce899885e032f5dc9c3d8
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-06-05 10:14:41 +0300

    feat(plugins): update authMiddleware

--- src/shared/plugins/auth.ts
+++ src/shared/plugins/auth.ts
@@ -1,23 +1,75 @@
 import {FastifyReply, FastifyRequest} from "fastify";
+import jwt from 'jsonwebtoken'
 
 export async function authMiddleware(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
+
   try {
-    console.log("[authMiddleware] -> cookie - ", request.cookies)
 
-    await request.jwtVerify({ onlyCookie: true });
-
-    console.log("[authMiddleware] -> user - ", request.user)
-    console.log("[authMiddleware] -> userId - ", request.user.userId)
-
-  } catch (error) {
-
-    request.log.error(error)
-
-    return reply.status(401).send({
-      error: 'Unauthorized'
+    // ACCESS TOKEN
+    await request.jwtVerify({
+      onlyCookie: true
     })
+
+  } catch {
+
+    const refreshToken =
+      request.cookies.refreshToken
+
+    if (!refreshToken) {
+
+      return reply.status(401).send({
+        message: 'Unauthorized'
+      })
+    }
+
+    try {
+
+      // REFRESH TOKEN
+      const payload =
+        jwt.verify(
+          refreshToken,
+          process.env.JWT_REFRESH_SECRET!
+        ) as {
+          userId: string
+          email: string
+        }
+
+      // NEW ACCESS TOKEN
+      const newAccessToken =
+        request.server.jwt.sign(
+          {
+            id: payload.userId,
+            email: payload.email,
+          },
+          {
+            expiresIn: '15m'
+          }
+        )
+
+      // NEW COOKIE
+      reply.setCookie(
+        'accessToken',
+        newAccessToken,
+        {
+          httpOnly: true,
+          secure:
+            process.env.NODE_ENV === 'production',
+          sameSite: 'lax',
+          path: '/',
+        }
+      )
+
+      // IMPORTANT
+      request.user = payload
+
+    } catch {
+
+      return reply.status(401).send({
+        message: 'Unauthorized'
+      })
+    }
   }
 }
