3c472e399a8c563e3b54b4804eb594d2d258392c
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-06-07 00:27:41 +0300

    feat(plugin): update authMiddleware

--- src/shared/plugins/auth.ts
+++ src/shared/plugins/auth.ts
@@ -1,5 +1,8 @@
 import {FastifyReply, FastifyRequest} from "fastify";
 import jwt from 'jsonwebtoken'
+import {AuthRepository} from "../../modules/auth/auth.repository";
+
+const repository = new AuthRepository()
 
 export async function authMiddleware(
   request: FastifyRequest,
@@ -37,6 +40,16 @@ export async function authMiddleware(
           email: string
         }
 
+      const dbSession = await repository.findSessionByToken(refreshToken);
+
+      if (!dbSession) {
+        // Если сессии в базе нет — значит токен отозван (был логаут).
+        // Принудительно чистим куки "зомби" и выплевываем 401.
+        reply.clearCookie('accessToken', { path: '/' });
+        reply.clearCookie('refreshToken', { path: '/' });
+        return reply.status(401).send({ message: 'Session expired or logged out' });
+      }
+
       // NEW ACCESS TOKEN
       const newAccessToken =
         request.server.jwt.sign(
@@ -67,9 +80,12 @@ export async function authMiddleware(
 
     } catch {
 
+      reply.clearCookie('accessToken', { path: '/' });
+      reply.clearCookie('refreshToken', { path: '/' });
+
       return reply.status(401).send({
         message: 'Unauthorized'
-      })
+      });
     }
   }
 }
