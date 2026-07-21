cb8ef43388b117a263c228ba1d3c90ad7254497a
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-07-21 18:11:04 +0300

    feat(modules): add findBySlug to ProductRepository

--- src/modules/product/product.repository.ts
+++ src/modules/product/product.repository.ts
@@ -1,8 +1,10 @@
-import { PrismaClient } from '../../generated/prisma/client';
-import { CreateInput, UpdateInput } from './product.types';
+import { Prisma, PrismaClient} from '../../generated/prisma/client';
+import {DefaultArgs} from "../../generated/prisma/runtime/client";
+import {CreateInput, UpdateInput} from './product.types';
 
 export class ProductRepository {
-  constructor(private readonly prisma: PrismaClient) {}
+  constructor(private readonly prisma: PrismaClient) {
+  }
 
   // Посмотреть поле slug для data (CreateInput)
   async create(data: CreateInput & { slug: string }) {
@@ -11,12 +13,20 @@ export class ProductRepository {
     });
   }
 
-  async findMany() {
-    return this.prisma.product.findMany();
+  async findMany(params?: { select?: Prisma.ProductSelect<DefaultArgs> | null | undefined; omit?: Prisma.ProductOmit<DefaultArgs> | null | undefined; include?: Prisma.ProductInclude<DefaultArgs> | null | undefined; where?: Prisma.ProductWhereInput | undefined; orderBy?: (Prisma.ProductOrderByWithRelationInput | Prisma.ProductOrderByWithRelationInput[]) | undefined; cursor?: Prisma.ProductWhereUniqueInput | undefined; take?: number | undefined; skip?: number | undefined; distinct?: (Prisma.ProductScalarFieldEnum | Prisma.ProductScalarFieldEnum[]) | undefined; } | undefined) {
+    return this.prisma.product.findMany(params);
   }
 
   async findById(id: string) {
     return this.prisma.product.findUnique({ where: { id } });
+  }
+
+  async findBySlug(slug: string) {
+    return this.prisma.product.findUnique({
+      where: {
+        slug,
+      },
+    });
   }
 
   async update(id: string, data: UpdateInput) {
