d264ab22c286753ce32e9995aae7e1207f54b2b7
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-07-21 18:11:22 +0300

    feat(modules): add get by slug to productRoutes

--- src/modules/product/product.routes.ts
+++ src/modules/product/product.routes.ts
@@ -11,6 +11,7 @@ const controller = new ProductController(service);
 export async function productRoutes(fastify: FastifyInstance) {
   fastify.get('/', controller.getAll);
   fastify.get('/:id', controller.getById);
+  fastify.get('/slug/:slug', controller.getBySlug);
   fastify.post('/', controller.create);
   fastify.patch('/:id', controller.update);
   fastify.delete('/:id', controller.delete);
